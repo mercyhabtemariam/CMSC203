1ae09c955d9b69a2544bd00de1d49f5e6cd8890f
Mercy habtemairam <mhabte10@montgomerycollege.edu>
2026-04-09 13:41:41 -0400

    Add files via upload

---
+++ CMSC203_Lab6/SavingsAccount.java
@@ -0,0 +1,31 @@
+public class SavingsAccount extends BankAccount
+{
+    private double rate = 0.025;
+    private int savingsNumber = 0;
+    private String accountNumber;
+
+    public SavingsAccount(String name, double amount)
+    {
+        super(name, amount);
+        accountNumber = super.getAccountNumber() + "-" + savingsNumber;
+    }
+
+    public SavingsAccount(SavingsAccount oldAccount, double amount)
+    {
+        super(oldAccount, amount);
+        this.savingsNumber = oldAccount.savingsNumber + 1;
+        this.accountNumber = super.getAccountNumber() + "-" + savingsNumber;
+    }
+
+    public void postInterest()
+    {
+        double monthlyInterest = getBalance() * rate / 12;
+        deposit(monthlyInterest);
+    }
+
+    @Override
+    public String getAccountNumber()
+    {
+        return accountNumber;
+    }
+}
